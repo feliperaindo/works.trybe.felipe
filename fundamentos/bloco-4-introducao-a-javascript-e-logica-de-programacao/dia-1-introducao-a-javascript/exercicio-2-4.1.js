ff198b339a1686291f970b25b04eb9b48d3bd6a3
feliperaindo <felipe.raindo@gmail.com>
2022-08-18 17:10:33 -0300

    finaliza exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicio-2-4.1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicio-2-4.1.js
@@ -1,9 +1,8 @@
-let patientId = "50";
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+const base = 5;
+let height = 8;
 
-console.log(typeof patientId);
+const area = base * height;
+
+const perimeter = base*2 + height*2;
+
+console.log(perimeter);
