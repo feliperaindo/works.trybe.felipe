55cac25990870b2e721363964a83daca69408145
feliperaindo <felipe.raindo@gmail.com>
2022-08-18 21:10:51 -0300

    finaliza até exercicio 6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicio-5.4.1.js
@@ -0,0 +1,9 @@
+
+let weekDay = "domingo";
+
+
+if (weekDay === "segunda-feira" || weekDay === "terca-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+    console.log ("FINALMENTE, descanso merecido UwU.");
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicio-6-4.1.js
@@ -0,0 +1,19 @@
+let candidato = "waiting";
+
+switch (candidato) {
+
+    case "aproved":
+        console.log ('Aprovada(o).');
+        break;
+
+    case "listed":
+        console.log ('Lista de espera.');
+        break;
+
+    case "reproved":
+        console.log ('Reprovada(o).');
+        break;
+    
+    default:
+        console.log ('Não se aplica.');
+};
