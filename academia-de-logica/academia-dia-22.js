30fb02c9e6ea5f2bf51fad39232fbf3eb2ad8f0a
feliperaindo <felipe.raindo@gmail.com>
2022-09-08 20:46:34 -0300

    corrige arquivo academia de lógica

--- academia-de-logica/academia-dia-22.js
+++ academia-de-logica/academia-dia-22.js
@@ -4,34 +4,34 @@
 
 //Solução em Grupo.
 let number = [12, 37, 42, 7, 120];
-let numeroEncontrado = [];
+let numeroEncontradoTwo = [];
 let search = 12;
 
 for (let i = 0; i < number.length; i += 1) {
 if(search === number[i]){
-numeroEncontrado.push(number[i]);
+numeroEncontradoTwo.push(number[i]);
 } 
 }
-console.table(numeroEncontrado);
+console.table(numeroEncontradoTwo);
 
-let numerosImpares = [];
+let numerosImparesTwo = [];
 
 for (let i = 1   ; i <= 50; i += 1) {
 if ((i % 2) == 1){
-numerosImpares.push(i);
+numerosImparesTwo.push(i);
 } 
 }
-console.table(numerosImpares);
+console.table(numerosImparesTwo);
 
 let numerosImpares = [];
 let numeroEncontrado = [];
 
 for (let i = 0; i < 155; i += 1) {
-if ((numerosImpares[i] % 3) == 0){
-numerosImpares.push(i);
+if ((numerosImparesTwo[i] % 3) == 0){
+numerosImparesTwo.push(i);
 } 
 }
-if (numerosImpares.length > 50) {
+if (numerosImparesTwo.length > 50) {
 console.log('Há 50 ou mais números divisiveis por 3.')
 } else {
 console.log('Sequência muito pequena.')
